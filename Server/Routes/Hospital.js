bb4883a5dab60765b6969af71c54e11c8c128b3b
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-09-30 07:41:14 +0530

    Adding put files

--- Server/Routes/Hospital.js
+++ Server/Routes/Hospital.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get('/', getHospitals); // Usage of get route to get all hospitals
 router.post('/', createHospital); // Usage of post route to create a new hospital
-router.put('/:id', updateHospital); // PUT update a hospital
+router.put('/:id', updateHospital); // Usage of put route to update a hospital
 router.delete('/:id', deleteHospital); // DELETE a hospital
 
 module.exports = router;
