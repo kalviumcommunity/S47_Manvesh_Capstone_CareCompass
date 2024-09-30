fef9dbded3b0a0a17331760743912e09a6b5acee
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-09-30 07:35:34 +0530

    Adding the file having get method

--- Server/Routes/Hospital.js
+++ Server/Routes/Hospital.js
@@ -4,33 +4,13 @@ const {
   createHospital,
   updateHospital,
   deleteHospital,
-} = require('../controllers/hospitalController');
+} = require('../Controllers/HospitalController');
 
 const router = express.Router();
 
-router.get('/', getHospitals); // GET all hospitals
+router.get('/', getHospitals); // Usage of get route to get all hospitals
 router.post('/', createHospital); // POST a new hospital
 router.put('/:id', updateHospital); // PUT update a hospital
 router.delete('/:id', deleteHospital); // DELETE a hospital
 
 module.exports = router;
-// const express = require('express');
-// const {
-//   getHospitals,
-//   createHospital,
-//   updateHospital,
-//   deleteHospital,
-// } = require('../controllers/hospitalController');
-// const { authMiddleware } = require('../Middilewares/authMiddleware'); // Import your existing JWT middleware
-
-// const router = express.Router();
-
-// // Public route to get all hospitals
-// router.get('/', getHospitals); // GET all hospitals
-
-// // Protected routes with authentication
-// router.post('/',  createHospital); // POST a new hospital
-// router.put('/:id', authMiddleware, updateHospital); // PUT update a hospital
-// router.delete('/:id', authMiddleware, deleteHospital); // DELETE a hospital
-
-// module.exports = router;
